c56a790fb49c23bc086707cec3c2a60d10f76812
Sarapena02 <sarapenagomez02@gmail.com>
2023-08-28 22:43:33 -0500

    Hice layouts de la pagina de inicio del Doc y la lista de pacientes

---
+++ app/src/main/java/com/icm/medifast/Perfil_Doc.kt
@@ -0,0 +1,11 @@
+package com.icm.medifast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Perfil_Doc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_doc)
+    }
+}
