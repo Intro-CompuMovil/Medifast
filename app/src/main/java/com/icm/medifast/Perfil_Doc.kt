42031dfe3f39113b6b7ca0c3f31618633efbbfce
ALEYI17 <aleelaleyi@gmail.com>
2023-08-30 23:50:10 -0500

    anadi un intent

--- app/src/main/java/com/icm/medifast/Perfil_Doc.kt
+++ app/src/main/java/com/icm/medifast/Perfil_Doc.kt
@@ -1,13 +1,23 @@
 package com.icm.medifast
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class Perfil_Doc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_doc)
+
+        val pasarDoctor = findViewById<Button>(R.id.botonPacientes)
+
+        pasarDoctor.setOnClickListener(){
+            val Pacientes = Intent(this,Pacientes::class.java)
+            startActivity(Pacientes)
+        }
 
     }
 }
