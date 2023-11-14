38c6f1c879e0359e0561c3ea99c2cbb74026222d
Andres-Salamanca <98924292+Andres-Salamanca@users.noreply.github.com>
2023-11-14 12:42:09 -0500

    ya esta la lista de pacientes

--- app/src/main/java/com/icm/medifast/Pacientes.kt
+++ app/src/main/java/com/icm/medifast/Pacientes.kt
@@ -6,19 +6,104 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.icm.medifast.databinding.ActivityCitasBinding
+import com.icm.medifast.databinding.ActivityPacientesBinding
+import com.icm.medifast.entities.Cita
+import com.icm.medifast.entities.Cliente
 
 class Pacientes : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
+    private lateinit var binding: ActivityPacientesBinding
+    private lateinit var adapter: ClienteAdapter
+    private lateinit var auth: FirebaseAuth
+    private val database = FirebaseDatabase.getInstance()
+    private lateinit var myRef: DatabaseReference
+
+    companion object{
+        lateinit var citaEscogida: Cita
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pacientes)
-        var paciente = findViewById<TextView>(R.id.nombre1)
+        //setContentView(R.layout.activity_pacientes)
 
-        val verHistorial = findViewById<Button>(R.id.historial1)
+        binding = ActivityPacientesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        verHistorial.setOnClickListener{
-            val Historial =  Intent(this, Historial::class.java)
-            startActivity(Historial)
-        }
+        auth = Firebase.auth
+
+        val PacienteList = mutableListOf<Cliente>()
+        adapter = ClienteAdapter(this, PacienteList)
+
+        binding.listCitas.adapter = adapter
+
+        getCitasForDoctor(auth.currentUser!!.uid)
+    }
+
+    private fun getCitasForDoctor(uid: String) {
+
+        val citasReference = database.reference.child("citas")
+
+        citasReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val citasList = mutableListOf<Cliente>()
+
+                for (citaSnapshot in dataSnapshot.children) {
+                    val cita = citaSnapshot.getValue(Cita::class.java)
+
+                    // Check if the cita is for the specified paciente ID
+                    if (cita?.idDoctor == uid) {
+                        // Fetch doctor and patient names based on their IDs
+                        fetchPatient(cita.idPaciente) { paciente ->
+
+                            // Update the Cita object with names
+                            cita.paciente = paciente
+                            cita.doctor = Perfil_Doc.myDoctor
+
+                            // Add the modified Cita to the list
+                            if (paciente != null) {
+                                citasList.add(paciente)
+                            }
+
+                            // Update the adapter with the new data
+                            adapter.clear()
+                            adapter.addAll(citasList)
+                            adapter.notifyDataSetChanged()
+
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle error
+            }
+        })
+
+
+
+    }
+
+
+    private fun fetchPatient(patientId: String, callback: (Cliente?) -> Unit) {
+        val clientsReference = database.reference.child("clientes").child(patientId)
+
+        clientsReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val patient = dataSnapshot.getValue(Cliente::class.java)
+                callback(patient)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle error
+            }
+        })
     }
 }
