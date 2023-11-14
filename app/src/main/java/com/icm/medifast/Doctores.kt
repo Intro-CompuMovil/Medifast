58fcd04a5014f9ae00e8b8303dc3c3a675973796
Andres-Salamanca <98924292+Andres-Salamanca@users.noreply.github.com>
2023-11-13 20:46:56 -0500

    actualize el constructor de doctores

--- app/src/main/java/com/icm/medifast/Doctores.kt
+++ app/src/main/java/com/icm/medifast/Doctores.kt
@@ -6,7 +6,32 @@ data class Doctores(
     val FotoResource: Int,
     val Eps:String
 ) {
+
     // Constructor sin argumentos necesario para Firebase
-    constructor() : this("", "", R.drawable.doctor2,"")
+    constructor() : this("", "", getRandomImageResource(), "")
+
+    companion object {
+        private val randomImageResources = arrayOf(
+            R.drawable.doctora1,
+            R.drawable.doctor2,
+            R.drawable.doctor3,
+            R.drawable.doctor4,
+            R.drawable.doctor5,
+            // Add more image resources as needed
+        )
+
+        fun getRandomImageResource(): Int {
+            // Select a random image resource from the array
+            return randomImageResources.random()
+        }
+    }
+
+    // Constructor que selecciona una imagen aleatoria
+    constructor(nombre: String, especialidad: String, eps: String) : this(
+        nombre,
+        especialidad,
+        getRandomImageResource(),
+        eps
+    )
 }
 
